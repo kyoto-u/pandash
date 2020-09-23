941f1ef65d2bb9bdc91f4ec5682bb4cb32c72ec2
RyosukeKita <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2020-09-23 16:33:21 +0900

    list

--- run.py
+++ run.py
@@ -17,7 +17,17 @@ def controller():
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    
+@app.route('/tasklist')
+def tasklist():
+    tasks = [
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'},
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'},
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'},
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'},
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'},
+        {'subject':'線形代数', 'taskname':'課題1', 'status':'未完了', 'time_left':'30', 'deadline':'0930'}           
+        ]
+    return flask.render_template('tasklist.htm',tasks=tasks)
 
 
 if __name__ =='__main__':
