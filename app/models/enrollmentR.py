b4cc40a7b39857d310f2bc69062fd9ad8cfd546f
RyosukeKita <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2020-10-21 17:25:01 +0900

    enrollmentR table

--- app/models/enrollmentR.py
+++ app/models/enrollmentR.py
@@ -8,9 +8,9 @@ class EnrollmentR(settings.Base):
     __tablename__ = 'enrollmentRs'
     EnrollmentID = sqlalchemy.Column(Integer(), primary_key=True, autoincrement=True)
     # ResourceID
-    ResourceUrl = sqlalchemy.Column(String(40))
+    ResourceUrl = sqlalchemy.Column(String(500))
     StudentID = sqlalchemy.Column(String(40))
     CourseID = sqlalchemy.Column(String(40))
-    Status = sqlalchemy.Column(String(40))
+    Status = sqlalchemy.Column(String(8))
 
 settings.Base.metadata.create_all(settings.engine)
