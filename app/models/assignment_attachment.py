060fe8b88e19cefe6c7c0f22e483802683574497
RyosukeKita <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2020-10-21 17:22:41 +0900

    assignment_attachment

--- app/models/assignment_attachment.py
+++ app/models/assignment_attachment.py
@@ -5,7 +5,7 @@ from .. import settings
 
 class Assignment_attachment(settings.Base):
     __tablename__ = 'assignment_attachments'
-    Url = Column(String(100), primary_key=True)
+    Url = Column(String(500), primary_key=True)
     Title = Column(String(100))
     AssignmentID = Column(String(40))
 
