f2d3514260209a1aa742192c55c869befaceddff
RyosukeKita <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2020-10-21 17:26:33 +0900

    resource table

--- app/models/resource.py
+++ app/models/resource.py
@@ -6,9 +6,9 @@ from .. import settings
 class Resource(settings.Base):
     __tablename__ = 'resources'
     # ResourceID = Column(Integer(), primary_key=True, autoincrement=True)
-    ResourceUrl = Column(String(100), primary_key=True)
-    Title = Column(String(40))
-    Container = Column(String(80))
+    ResourceUrl = Column(String(500), primary_key=True)
+    Title = Column(String(100))
+    Container = Column(String(500))
     ModifiedDate = Column(sqlalchemy.BigInteger())
 
 settings.Base.metadata.create_all(settings.engine)
