4cc3b92aad8877dc6c7c7ef59a0af623740a48cb
RyosukeKita <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2020-10-21 17:23:53 +0900

    course table

--- app/models/course.py
+++ app/models/course.py
@@ -8,6 +8,9 @@ class Course(settings.Base):
     CourseID = Column(String(40), primary_key=True)
     InstructorID = Column(String(40))
     CourseName = Column(String(40))
-    ClassSchedule = Column(String(40))
+    # 2020年前期：20200 2020年後期：20201
+    YearSemester = Column(Integer())
+    # mon1, wed2, ...   othe
+    ClassSchedule = Column(String(4))
 
 settings.Base.metadata.create_all(settings.engine)
