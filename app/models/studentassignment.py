7501d673d468e95e487f11976eea7732ce0d38b8
Nakai <nakai.tomoya.56n@st.kyoto-u.ac.jp>
2020-12-02 17:28:11 +0900

    テーブルにindexを貼る

--- app/models/studentassignment.py
+++ app/models/studentassignment.py
@@ -8,7 +8,7 @@ class Student_Assignment(settings.Base):
     __tablename__ = 'studentassignments'
     sa_id = sqlalchemy.Column(Integer(), primary_key=True, autoincrement=True)
     assignment_id = sqlalchemy.Column(String(40))
-    student_id = sqlalchemy.Column(String(40))
+    student_id = sqlalchemy.Column(String(40),index=True)
     status = sqlalchemy.Column(String(8))
 
 settings.Base.metadata.create_all(settings.engine)
--- app/models/studentresource.py
+++ app/models/studentresource.py
@@ -8,7 +8,7 @@ class Student_Resource(settings.Base):
     __tablename__ = 'studentresources'
     sr_id = sqlalchemy.Column(Integer(), primary_key=True, autoincrement=True)
     resource_url = sqlalchemy.Column(String(500))
-    student_id = sqlalchemy.Column(String(40))
+    student_id = sqlalchemy.Column(String(40),index=True)
     status = sqlalchemy.Column(Integer())
 
 settings.Base.metadata.create_all(settings.engine)
