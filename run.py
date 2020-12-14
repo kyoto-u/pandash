41560b920297b97561f8fe0575609a98dd88fcd0
{赤山慎治} <{earthmiran@gmail.com}>
2020-12-14 18:15:31 +0900

    12.14_2

--- run.py
+++ run.py
@@ -291,5 +291,5 @@ def pgtCallback():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    #app.run(debug=True, host='0.0.0.0', port=80)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
