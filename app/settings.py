932626e953a9f9a73b34b7f834e8895e4e0fab44
{赤山慎治} <{earthmiran@gmail.com}>
2020-12-14 17:58:32 +0900

    12.14

--- app/settings.py
+++ app/settings.py
@@ -11,10 +11,8 @@ proxy_callback = 'http://158.101.137.245/login/proxy'
 # 'https://cas.ecs.kyoto-u.ac.jp/cas/proxy?targetService=http://localhost/proxy&pgt=PGT-330-CSdUc5fCBz3g8KDDiSgO5osXfLMj9sRDAI0xDLg7jPn8gZaDqS'
 # cas_client = CASClient(cas_url, auth_prefix='', proxy_url=proxy_url, proxy_callback=proxy_callback)
 
-#engine = create_engine(
- #   'mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
- engine = create_engine(
-     'mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
+#engine = create_engine('mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
+engine = create_engine('mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
 
 session = orm.scoped_session(
     orm.sessionmaker(bind=engine)
