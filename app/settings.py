b2b5b873d28b7807dcdd7814440ae0f800411726
{赤山慎治} <{earthmiran@gmail.com}>
2020-12-28 16:58:58 +0900

    コンフリクトの解消#

--- app/settings.py
+++ app/settings.py
@@ -10,8 +10,8 @@ proxy_callback = 'https://pandax.ecs.kyoto-u.ac.jp/sakai-login-tool/container'
 
 cas_client = CASClient(cas_url, auth_prefix='', proxy_url=proxy_url, proxy_callback=proxy_callback)
 
-engine = create_engine('mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
-#engine = create_engine('mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
+#engine = create_engine('mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
+engine = create_engine('mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
 
 session = orm.scoped_session(
     orm.sessionmaker(bind=engine)
