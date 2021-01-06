88f94d94faa3d5bab3b95744b5c6041819a2d3a2
montplusa <toomynakai@gmail.com>
2021-01-06 18:29:56 +0900

    run.py内の記述をまとめる

--- run.py
+++ run.py
@@ -223,52 +223,24 @@ def overview():
 
 @app.route('/tasklist/day/<day>')
 def tasklist_day_redirect(day):
-    return flask.redirect(flask.url_for('tasklist_day', day = day,show_only_unfinished = 0,max_time_left = 3))
+    return tasklist_day(day,0,3)
 
 @app.route('/tasklist/course/<courseid>')
 def tasklist_course_redirect(courseid):
-    return flask.redirect(flask.url_for('tasklist_course', courseid = courseid,show_only_unfinished = 0,max_time_left = 3))
+    return tasklist_course(courseid,0,3)
 
 @app.route('/tasklist/day/<day>/<int:show_only_unfinished>/<int:max_time_left>')
 def tasklist_day(day,show_only_unfinished,max_time_left):
-    studentid = "student1"
-    tasks = get_tasklist(studentid ,day = day)
-    tasks = sort_tasks(tasks, show_only_unfinished = show_only_unfinished, max_time_left = max_time_left)
-    data ={"others":[]}
-    data = setdefault_for_overview(studentid)
-    search_condition = get_search_condition(show_only_unfinished, max_time_left, day=day)
-    return flask.render_template('tasklist.htm', tasks=tasks, data=data, day=day, search_condition=search_condition)
+    return tasklist_general(show_only_unfinished,max_time_left, day=day)
 
 
 @app.route('/tasklist/course/<courseid>/<int:show_only_unfinished>/<int:max_time_left>')
 def tasklist_course(courseid,show_only_unfinished,max_time_left):
-    studentid = "student1"
-    tasks = get_tasklist(studentid ,courseid = courseid)
-    tasks = sort_tasks(tasks, show_only_unfinished = show_only_unfinished, max_time_left = max_time_left)
-
-    data ={"others":[]}
-    data = setdefault_for_overview(studentid)
-    search_condition = get_search_condition(show_only_unfinished, max_time_left, course=courseid)
-    return flask.render_template('tasklist.htm', tasks=tasks, data=data, day='oth', search_condition=search_condition)
+    return tasklist_general(show_only_unfinished,max_time_left, courseid = courseid)
 
 @app.route('/tasklist/<int:show_only_unfinished>/<int:max_time_left>')
 def tasklist(show_only_unfinished,max_time_left):
-    studentid = "student1"
-    tasks = get_tasklist(studentid)
-    # tasks = [
-    #     {'subject':'[2020前期月1]線形代数学', 'classschedule':'mon1','taskname':'課題1', 'status':'未', 'time_left': "あと50分", 'deadline':'2020-10-30T00:50:00Z','instructions':'なし'},
-    #     {'subject':'[2020前期月1]線形代数学', 'classschedule':'mon1','taskname':'課題2', 'status':'未', 'time_left':'あと2時間', 'deadline':'2020-10-30T02:00:00Z','instructions':'なし'},
-    #     {'subject':'[2020前期月2]微分積分学', 'classschedule':'mon2','taskname':'課題3', 'status':'終了', 'time_left':'', 'deadline':'2020-10-29T23:00:00Z','instructions':'なし'},
-    #     {'subject':'[2020前期火1]英語リーディング', 'classschedule':'Tue1','taskname':'課題4', 'status':'未', 'time_left':'あと3日', 'deadline':'2020-11-02T03:00:00Z','instructions':'なし'},
-    #     {'subject':'[2020前期月2]微分積分学', 'classschedule':'mon2','taskname':'課題5', 'status':'済', 'time_left':'あと1時間', 'deadline':'2020-10-30T01:00:00Z','instructions':'なし'},
-    #     {'subject':'[2020前期月1]英語ライティングリスニング', 'classschedule':'mon1','taskname':'課題6', 'status':'済', 'time_left':'あと1日', 'deadline':'2020-10-31T01:00:00Z','instructions':'なし'}
-    #     ]
-    tasks = sort_tasks(tasks, show_only_unfinished = show_only_unfinished, max_time_left = max_time_left)
-
-    data ={"others":[]}
-    data = setdefault_for_overview(studentid)
-    search_condition = get_search_condition(show_only_unfinished, max_time_left)
-    return flask.render_template('tasklist.htm', tasks=tasks, data=data, day='oth', search_condition=search_condition)
+    return tasklist_general(show_only_unfinished,max_time_left)
 
 @app.route('/resourcelist/course/<courseid>')
 def resource_course(courseid):
@@ -346,6 +318,36 @@ def pgtCallback():
         pgt = request.form
         print(pgt)
         return ''
+
+
+def tasklist_general(show_only_unfinished,max_time_left,day = None,courseid = None):
+    studentid = "student1"
+    if courseid != None:
+        tasks = get_tasklist(studentid,courseid=courseid)
+    elif day != None:
+        tasks = get_tasklist(studentid,day=day)
+    else:
+        tasks = get_tasklist(studentid)
+    # tasks = [
+    #     {'subject':'[2020前期月1]線形代数学', 'classschedule':'mon1','taskname':'課題1', 'status':'未', 'time_left': "あと50分", 'deadline':'2020-10-30T00:50:00Z','instructions':'なし'},
+    #     {'subject':'[2020前期月1]線形代数学', 'classschedule':'mon1','taskname':'課題2', 'status':'未', 'time_left':'あと2時間', 'deadline':'2020-10-30T02:00:00Z','instructions':'なし'},
+    #     {'subject':'[2020前期月2]微分積分学', 'classschedule':'mon2','taskname':'課題3', 'status':'終了', 'time_left':'', 'deadline':'2020-10-29T23:00:00Z','instructions':'なし'},
+    #     {'subject':'[2020前期火1]英語リーディング', 'classschedule':'Tue1','taskname':'課題4', 'status':'未', 'time_left':'あと3日', 'deadline':'2020-11-02T03:00:00Z','instructions':'なし'},
+    #     {'subject':'[2020前期月2]微分積分学', 'classschedule':'mon2','taskname':'課題5', 'status':'済', 'time_left':'あと1時間', 'deadline':'2020-10-30T01:00:00Z','instructions':'なし'},
+    #     {'subject':'[2020前期月1]英語ライティングリスニング', 'classschedule':'mon1','taskname':'課題6', 'status':'済', 'time_left':'あと1日', 'deadline':'2020-10-31T01:00:00Z','instructions':'なし'}
+    #     ]
+    tasks = sort_tasks(tasks, show_only_unfinished = show_only_unfinished, max_time_left = max_time_left)
+
+    data ={"others":[]}
+    data = setdefault_for_overview(studentid)
+    if courseid != None:
+        search_condition = get_search_condition(show_only_unfinished, max_time_left, courseid=courseid)
+    elif day != None:
+        search_condition = get_search_condition(show_only_unfinished, max_time_left, day=day)
+        return flask.render_template('tasklist.htm', tasks=tasks, data=data, day=day, search_condition=search_condition)
+    else:
+        search_condition = get_search_condition(show_only_unfinished, max_time_left)
+    return flask.render_template('tasklist.htm', tasks=tasks, data=data, day='oth', search_condition=search_condition)
 
 
 if __name__ == '__main__':
