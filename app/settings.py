f6ca2885be84868970470bc0aebae5dc99f87028
{赤山慎治} <{earthmiran@gmail.com}>
2021-01-12 17:46:25 +0900

    finished_boxの数字が横並びになるよう調整

--- app/settings.py
+++ app/settings.py
@@ -11,8 +11,8 @@ api_url = 'https://panda.ecs.kyoto-u.ac.jp/direct/'
 
 cas_client = CASClient(cas_url, auth_prefix='', proxy_url=proxy_url, proxy_callback=proxy_callback)
 
-# engine = create_engine('mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
-engine = create_engine('mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
+engine = create_engine('mysql://pandash:pandash@localhost/pandash00?charset=utf8', echo=False)
+#engine = create_engine('mysql://root:9F#Mhyx3Zfxa@localhost/pandash?charset=utf8', echo=False)
 
 session = orm.scoped_session(
     orm.sessionmaker(bind=engine)
