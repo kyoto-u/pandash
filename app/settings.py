7c24f5a6368e7c3ed247ef599b8dbbbc59da7a7b
montplusa <toomynakai@gmail.com>
2021-02-02 22:03:43 +0900

    settings.pyの変数を変更する

--- app/settings.py
+++ app/settings.py
@@ -2,13 +2,20 @@ from sqlalchemy import create_engine, orm
 from sqlalchemy.ext.declarative import declarative_base
 from cas_client import CASClient
 
+# このサービス自身のURL
 app_url ='https://pandash.ecs.kyoto-u.ac.jp'
-app_login_url = 'https://pandash.ecs.kyoto-u.ac.jp/login'
-app_logout_url = 'https://pandash.ecs.kyoto-u.ac.jp/logout'
+app_login_url = app_url + '/login'
+app_logout_url = app_url + '/logout'
 cas_url = 'https://cas.ecs.kyoto-u.ac.jp/cas'
-proxy_url = 'https://pandash.ecs.kyoto-u.ac.jp/pgtCallback'
+proxy_url = app_url + '/pgtCallback'
 proxy_callback = 'https://panda.ecs.kyoto-u.ac.jp/sakai-login-tool/container'
+# PandAのAPI用URL
 api_url = 'https://panda.ecs.kyoto-u.ac.jp/direct/'
+
+# 収集する教科の学期一覧
+# 前４ケタ:年度
+# 最後の一桁: 0-> 前期  1-> 後期  2-> 集中・通年など  3-> どれも取得できなかった場合
+VALID_YEAR_SEMESTER=[20201,20202]
 
 cas_client = CASClient(cas_url, auth_prefix='', proxy_url=proxy_url, proxy_callback=proxy_callback)
 
