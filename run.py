58dac956f8516b62b9f1dd1454e0f82f32b162cc
montplusa <toomynakai@gmail.com>
2021-02-09 20:24:59 +0900

    設定、授業資料ページで課題更新時に、自動的にもとのページにリダイレクトされるようにする

--- run.py
+++ run.py
@@ -171,8 +171,7 @@ def update_subject():
     studentid = session.get('student_id')
     if studentid:
         update_student_needs_to_update_sitelist(studentid,need_to_update_sitelist=1)
-        redirect_pages[studentid]='option'
-        return redirect(url_for('login'))
+        return redirect(url_for('login'),page='option')
     else:
         return redirect(url_for('login'))
 
