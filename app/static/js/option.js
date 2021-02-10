6a5b6e97c796fc4d089d43d444e51a505cdf6e8b
kitaryot <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2021-02-10 17:49:01 +0900

    設定ページ 適用したときのメッセージの追加

--- app/static/js/option.js
+++ app/static/js/option.js
@@ -12,6 +12,7 @@ $('#apply_btn').on('click', function() {
         contentType: 'application/json',
         success: function (response) {
             console.log(response);
+            $('apply_msg').show('0.4')
         },
         error: function (error) {
             console.log(error);
