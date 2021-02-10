ad4a24f2a05a3289799dae339055b12ea3ff2fd1
kitaryot <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2021-02-10 20:30:39 +0900

    設定ページで表示科目を選択できるようにする

--- app/static/js/option.js
+++ app/static/js/option.js
@@ -1,42 +1,3 @@
-$('#apply_btn').on('click', function() {
-    var show_already_due = 0
-    if ($('#show_already_due_check').prop('checked')){
-        show_already_due = 1
-    }
-    var show_course_list = new Array();
-    var hide_course_list = new Array();
-    $(".course_checkbox").change(function(){
-        var prop = $(this).prop('checked')
-        $(this)
-        if(prop){
-            show_course_list.push($(this).val);
-        }else{
-            hide_course_list.push($(this).val);
-        }
-    })
-
-    var settings = JSON.stringify({ "show_already_due": show_already_due, "show_course_list":show_course_list, "hide_course_list":hide_course_list});
-    $.ajax({
-        type: 'POST',
-        url: '/settings_change',
-        data: settings,
-        contentType: 'application/json',
-        success: function (response) {
-            console.log(response);
-            $('#cancel_msg').fadeOut();
-            $('#apply_msg').fadeOut();
-            $('#apply_msg').show('0.4');
-        },
-        error: function (error) {
-            console.log(error);
-            $('#apply_msg').fadeOut();
-            $('#cancel_msg').fadeOut();
-            $('#cancel_msg').show('0.4');
-        }
-    });
-})
-
-
 $('#show_already_due_check').change(function(){
     var show_already_due = 0;
     if($(this).prop('checked')){
@@ -70,7 +31,7 @@ $(".course_checkbox").change(function(){
     if(prop){
         hide = 0;
     }
-    course_list.push($(this).val);
+    course_list.push($(this).val());
     data = JSON.stringify({"course_list":course_list, "hide":hide})
     $.ajax({
         type: 'POST',
