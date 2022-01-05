af3632027152d0c651432a8a4fe8cdc5eb881fcb
shirokumaa <earthmiran@gmail.com>
2022-01-05 16:57:38 +0900

    test

--- app/akayama.py
+++ app/akayama.py
@@ -6,11 +6,23 @@ from pprint import pprint
 app = Flask(__name__)
 
 
+def button_clicked(button_name):
+    if button_name == "button1":
+        return "button1 clicked"
+    elif button_name == "button2":
+        return "button2 clicked"
+    else:
+        return "No button clicked"
+
+app.jinja_env.globals['button_clicked'] = button_clicked
+
 
 @app.route('/<name>')
 def main(name):
     templname = name + '.htm'
-    return render_template(templname, path=name)
+    announce={"title":"OAサンプル","subject":"サイト１","publish_date":"2021/12/27","html_file":"<h1>hogehoge</h1>"}
+
+    return render_template(templname, path=name, announce=announce, button_clicked = button_clicked)
 
 @app.route('/')
 def root():
@@ -18,3 +30,5 @@ def root():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
