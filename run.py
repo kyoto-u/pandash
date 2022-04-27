4c6bff17470393e2e0f162134a2d5c38b26c5558
montplusa <toomynakai@gmail.com>
2022-04-27 13:11:40 +0900

    CASなしでログインできるようにする

--- run.py
+++ run.py
@@ -1001,4 +1001,7 @@ if __name__ == '__main__':
     app.logger.setLevel(logging.INFO)
     app.logger.addHandler(log_handler)
     pgtids={}
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    redirect_pages={}
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
