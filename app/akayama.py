c97529b43e2fa25abb0100943a2c70a299e5c3d7
shirokumaa <earthmiran@gmail.com>
2022-08-01 15:59:14 +0900

    ダークテーマの実装

--- app/akayama.py
+++ app/akayama.py
@@ -10,7 +10,31 @@ app = Flask(__name__)
 @app.route('/<name>')
 def main(name):
     templname = name + '.htm'
-    return render_template(templname, path=name)
+    darktheme = True
+    search_condition = "全て"
+    sampletask1 = {"status": "未", 
+                    "assignment_url": "#", 
+                    "assignmentid": 1,
+                    "time_left": {"msg": "2日", "judge": "two_day"}, 
+                    "deadline": "2022/3/2",
+                    "subject":"サンプルサイト",
+                    "taskname":"サンプル課題１"
+                    }
+    tasks = [sampletask1]
+    last_update = "2020/3/2"
+    data = {}
+    for i in range(1,6):
+        data["mon"+str(i)] = {"searchURL": "#", "shortname": "サンプル月"+str(i)}
+        data["tue"+str(i)] = {"searchURL": "#", "shortname": "サンプル火"+str(i)}
+        data["wed"+str(i)] = {"searchURL": "#", "shortname": "サンプル水"+str(i)}
+        data["thu"+str(i)] = {"searchURL": "#", "shortname": "サンプル木"+str(i)}
+        data["fri"+str(i)] = {"searchURL": "#", "shortname": "サンプル金"+str(i)}
+    data["mon1"]["tasks"] = [sampletask1]
+    data["others"] = [{"shortname": "サンプルその他科目", "tasks": [sampletask1, sampletask1]}, {
+        "shortname": "サンプルその他科目", "tasks": [sampletask1, sampletask1]}]
+    with open("./test_html.txt",encoding="utf-8") as f:
+        html = f.read()
+    return render_template(templname,html=html, numofcourses=2,path=name, data=data,darktheme = darktheme,search_condition=search_condition,tasks = tasks,last_update = last_update,num=20,per_page=10,page=1)
 
 @app.route('/')
 def root():
---
+++ app/static/js/darktheme.js
@@ -0,0 +1,56 @@
+﻿$("document").ready(function(){
+    //共通部分
+    logo_loc = $("#siteface").attr("src")
+    logo_loc = logo_loc.replace("PandAsh_logo","PandAsh_logo_dark")
+    $("#siteface").attr("src",logo_loc)
+    $(".navbar-light").addClass("navbar-dark bg-dark")
+    $(".navbar-light").removeClass("navbar-light bg-light")
+    $(".border-dark").addClass("border-light")
+    $(".navbar-brand").css("color","white")
+    $(".border-dark").removeClass("border-dark")
+    $(".btn-light").addClass("btn-dark")
+    $(".btn-light").removeClass("btn-light")
+    $(".btn-dark").addClass("btn-light")
+    $(".btn-dark").removeClass("btn-dark")
+    $('body').css('background-color', '#3e4347');
+    $('body').css('color', 'white');
+    $('#footercont').css({ "background-color": '#3e4347' });
+    $(".card-header").css({ "color": "white", "background-color":"#3e4347"})
+    $(".faqque").css("color","white")
+    $(".card-body").css("background-color","#343a40")
+
+    //設定
+    $(".option-content").css("color", "black")
+    $(".toggle-on").removeClass("btn-light")
+    $(".toggle-on").addClass("btn-dark")
+    $("#trial-running").css({"color":"black","background-color":"whitesmoke"})
+    
+    // 課題_一覧表示
+    $(".fa-dumpster").css({"color":"white"})
+    $(".table").css({"color":"white"})
+    $(".task_link").css({ "color":"dodgerblue"})
+    $(".target_select").css({ "background-color":"dimgray"})
+    $(".task_selected_page").css({"color":"dimgray"})
+    $(".task_another_page").css({ "color":"whitesmoke"})
+
+    //課題_時間割表示
+    $(".striped_gray").css({ "background-color":"#474d52"})
+    $(".col-2>.course").css({"color":"deepskyblue"})
+    $(".card.col-md-3").css({"padding-left":"0","padding-right":"0"})
+
+    //授業資料
+    $(".fa-folder>a").css({"color":"skyblue"})
+
+    //モーダル
+    $(".modal-content").css({ "background-color": "#343a40" })
+    $("td>a").css({"color":"skyblue"})
+
+    $("p>a").css({ "color": "skyblue" })
+    
+
+    //設定
+    $(".option-content").css({ "color": "white", "background-color": "#2d3338" })
+
+    //お知らせ
+    $("th>a").css({"color":"white"})
+});
